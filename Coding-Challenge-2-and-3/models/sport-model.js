b9f4136b1f11d80342067fc56899982fbeec1dea
Mario Roque <A01282301@itesm.mx>
2020-05-11 08:27:55 -0500

    Code 2 and 3

--- Coding-Challenge-2-and-3/models/sport-model.js
+++ Coding-Challenge-2-and-3/models/sport-model.js
@@ -2,6 +2,42 @@ const mongoose = require( 'mongoose' );
 
 /* Your code goes here */
 
+//Create the schema
+const sportsSchema = mongoose.Schema({
+    id: { //Id must be unique
+        type : Number,
+        required : true,
+        unique : true
+    },
+    name : {
+        type : String,
+        required: true
+    },
+    num_players : {
+        type : String,
+        required : true
+
+    }
+})
+
+//We create the collection for the database
+const sportCollection = mongoose.model('sports', sportsSchema);
+
+
+//Here we create the endpoint function list
+//Adds the new created sport to the database
+const functionlist = {
+    addSport: function (name, num_players, id) {
+        let newsport = {id, name, num_players};
+        return sportCollection.create(newSport).then( added =>{
+            return added;
+        }).catch( err => {
+            return err;
+        })
+    }
+}
+
+
 module.exports = {
-    
+    functionlist
 };
--- Coding-Challenge-2-and-3/server.js
+++ Coding-Challenge-2-and-3/server.js
@@ -3,18 +3,57 @@ const bodyParser = require( 'body-parser' );
 const mongoose = require( 'mongoose' );
 const jsonParser = bodyParser.json();
 const { DATABASE_URL, PORT } = require( './config' );
-
+const morgan = require( 'morgan' );
+const model = require('./models/sport-model.js')
+const functions= {functionlist}
 const app = express();
-
-
+// I must send on the body: name, num_players, and the id
 /* Your code goes here */
+app.use(morgan('dev'));
+app.post('sports/addSport/:sportId', jsonParser, (req,res) =>{
+    
+    let name = req.body.name;
+    let num_players = req.body.num_players;
+    let id = req.body.num_players;
+    let sportid = res.header.sportid;
+    if(!name || !num_players || !id){
+        res.statusMessage = "A parameter is missing (name, num_players, id)";
+        return res.status(406).end();
+    }
+    if(id === sportid ){
+
+        let flag = false;
+
+        sports.forEach(element => {
+            if(element.id == sportid){
+                flag = true;
+            }
+        });
+        if(flag == true){
+            res.statusMessage = "Sport id already added";
+         return res.status(400).end();
+        }
+         return res.status(201).end();
+
+    }else{
+        functions.addSport(name, num_players, id);
+        res.statusMessage = "ID doesnt match";
+        return res.status(409).end();
+    }
+
+
+
+
+    console.log("name");
+})
+
 
 
 app.listen( PORT, () => {
     console.log( "This server is running on port 8080" );
     new Promise( ( resolve, reject ) => {
         const settings = {
-            useNewUrlParser: true, 
+           useNewUrlParser: true, 
             useUnifiedTopology: true, 
             useCreateIndex: true
         };
